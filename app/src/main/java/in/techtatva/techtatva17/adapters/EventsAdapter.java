63bc20fb1f6c842191cab5c16e34a711334080a7
Rahul Sathanapalli <skvrahul@gmail.com>
2017-06-14 10:01:03 +0530

    Adds Event to RealmDB and displays visual cues(icon and snackbar) when Favourite icon is clicked in Events Page

--- app/src/main/java/in/techtatva/techtatva17/adapters/EventsAdapter.java
+++ app/src/main/java/in/techtatva/techtatva17/adapters/EventsAdapter.java
@@ -1,5 +1,8 @@
 package in.techtatva.techtatva17.adapters;
 
+import android.app.usage.UsageEvents;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +11,17 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import in.techtatva.techtatva17.R;
 import in.techtatva.techtatva17.models.events.EventDetailsModel;
 import in.techtatva.techtatva17.models.events.EventsListModel;
 import in.techtatva.techtatva17.models.events.ScheduleListModel;
 import in.techtatva.techtatva17.models.events.ScheduleModel;
+import in.techtatva.techtatva17.models.favourites.FavouritesModel;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * Created by skvrahul on 30/5/17.
@@ -24,7 +33,11 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventViewH
     private final EventClickListener eventListener;
     private final FavouriteClickListener favouriteListener;
     private final EventLongPressListener eventLongPressListener;
+    private Realm realm = Realm.getDefaultInstance();
+    private RealmResults<FavouritesModel> favouritesRealm = realm.where(FavouritesModel.class).findAll();
+    private List<FavouritesModel> favourites = realm.copyFromRealm(favouritesRealm);
 
+//    private List<FavouritesModel> favouritesModelList = new ArrayList<>();
 
     //Interfaces to ClickListener of the item and of the Favourite Icon in the item
     public interface EventClickListener {
@@ -40,38 +53,44 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventViewH
 
     public class EventViewHolder extends RecyclerView.ViewHolder{
         public TextView eventName, eventVenue, eventTime;
-        public ImageView eventIcon, fav;
+        public ImageView eventIcon, favIcon;
         public RelativeLayout eventItem;
         public EventViewHolder(View view){
             super(view);
             eventIcon = (ImageView)view.findViewById(R.id.event_logo_image_view);
-            fav = (ImageView)view.findViewById(R.id.event_fav_ico);
+            favIcon = (ImageView)view.findViewById(R.id.event_fav_ico);
             eventName = (TextView)view.findViewById(R.id.event_name_text_view);
             eventVenue = (TextView)view.findViewById(R.id.event_venue_text_view);
             eventTime = (TextView)view.findViewById(R.id.event_time_text_view);
             eventItem = (RelativeLayout)view.findViewById(R.id.event_item_relative_layout);
         }
         public void onBind(final ScheduleModel event, final EventClickListener eventListener,final EventLongPressListener eventLongPressListener, final FavouriteClickListener favouriteListener){
-            eventName.setText(event.getEventName());
-
-
-            eventTime.setText(event.getStartTime() + " - " + event.getEndTime());
-            eventIcon.setImageResource(R.drawable.ic_sample_image_24dp);
-            eventVenue.setText(event.getVenue());
             //Individual OnClickListeners for the Favourite Icon and the entire Item
-            fav.setOnClickListener(new View.OnClickListener() {
+            favIcon.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     favouriteListener.onItemClick(event);
+                    if(favIcon.getTag().equals("deselected")) {
+                        favIcon.setImageResource(R.drawable.ic_fav_selected);
+                        favIcon.setTag("selected");
+                        addFavourite(event);
+                        Snackbar.make(v, event.getEventName()+" Added to Favourites", Snackbar.LENGTH_LONG).show();
+                    }else{
+                        favIcon.setImageResource(R.drawable.ic_fav_deselected);
+                        favIcon.setTag("deselected");
+                        removeFavourite(event);
+                        Snackbar.make(v, event.getEventName()+" removed from Favourites", Snackbar.LENGTH_LONG).show();
+                    }
+
                 }
             });
             eventItem.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     eventListener.onItemClick(event);
+
                 }
             });
-
             eventItem.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
@@ -79,6 +98,17 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventViewH
                     return false;
                 }
             });
+            eventName.setText(event.getEventName());
+            eventTime.setText(event.getStartTime() + " - " + event.getEndTime());
+            eventIcon.setImageResource(R.drawable.ic_sample_image_24dp);
+            eventVenue.setText(event.getVenue());
+            if(favouritesContainsEvent(event)){
+                favIcon.setImageResource(R.drawable.ic_fav_selected);
+                favIcon.setTag("selected");
+            }else{
+                favIcon.setImageResource(R.drawable.ic_fav_deselected);
+                favIcon.setTag("deselected");
+            }
 
         }
     }
@@ -96,6 +126,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventViewH
     public EventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_event, parent, false);
+
         return new EventViewHolder(itemView);
 
     }
@@ -109,5 +140,61 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventViewH
     @Override
     public int getItemCount() {
         return eventList.getData().size();
+    }
+
+    private void addFavourite(ScheduleModel eventSchedule){
+
+
+        FavouritesModel favourite = new FavouritesModel();
+
+        //Get Corresponding EventDetailsModel from Realm
+        EventDetailsModel eventDetails = realm.where(EventDetailsModel.class).equalTo("eventID",eventSchedule.getEventID()).findFirst();
+
+        //Create and Set Values for FavouritesModel
+        favourite.setId(eventSchedule.getEventID());
+        favourite.setCatID(eventSchedule.getCatID());
+        favourite.setEventName(eventSchedule.getEventName());
+        favourite.setRound(eventSchedule.getRound());
+        favourite.setVenue(eventSchedule.getVenue());
+        favourite.setDate(eventSchedule.getDate());
+        favourite.setDay(eventSchedule.getDay());
+        favourite.setStartTime(eventSchedule.getStartTime());
+        favourite.setEndTime(eventSchedule.getEndTime());
+
+        favourite.setParticipants(eventDetails.getMaxTeamSize());
+        favourite.setContactName(eventDetails.getContactName());
+        favourite.setContactNumber(eventDetails.getContactNo());
+        favourite.setCatName(eventDetails.getCatName());
+        favourite.setDescription(eventDetails.getDescription());
+        //Commit to Realm
+        realm.beginTransaction();
+        realm.copyToRealm(favourite);
+        realm.commitTransaction();
+
+        favourites.add(favourite);
+
+    }
+    private void removeFavourite(ScheduleModel eventSchedule){
+        realm.beginTransaction();
+        realm.where(FavouritesModel.class).equalTo("id",eventSchedule.getEventID()).equalTo("day",eventSchedule.getDay()).findAll().deleteAllFromRealm();
+        realm.commitTransaction();
+
+        for(FavouritesModel favourite : favourites){
+            //Removing corresponding FavouritesModel from favourites
+            if((favourite.getId().equals(eventSchedule.getEventID()))&&(favourite.getDay().equals(eventSchedule.getDay()))){
+                favourites.remove(favourite);
+
+            }
+        }
+    }
+    private boolean favouritesContainsEvent(ScheduleModel eventSchedule){
+        for(FavouritesModel favourite : favourites){
+            //Checking if Corresponding Event exists
+            if((favourite.getId().equals(eventSchedule.getEventID()))&&(favourite.getDay().equals(eventSchedule.getDay()))){
+                return true;
+
+            }
+        }
+        return false;
     }
 }
