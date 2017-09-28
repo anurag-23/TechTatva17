f719efec623d6a765547f188a76a4c4061a0076f
Rahul Sathanapalli <skvrahul@gmail.com>
2017-09-28 23:31:23 +0530

    Enlarges meme on clicking and removes CardView in EasterEgg layout

--- app/src/main/java/in/tt/tt17/adapters/EasterEggAdapter.java
+++ app/src/main/java/in/tt/tt17/adapters/EasterEggAdapter.java
@@ -1,7 +1,10 @@
 package in.tt.tt17.adapters;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,9 +36,15 @@ public class EasterEggAdapter extends RecyclerView.Adapter<EasterEggAdapter.East
 
     @Override
     public void onBindViewHolder(EasterEggAdapter.EasterEggViewHolder holder, int position) {
-        String link = eggList[position];
-
+        final String link = eggList[position];
+        holder.egg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayImagePopup(link);
+            }
+        });
         Picasso.with(context).load(link).into(holder.egg);
+
     }
 
     @Override
@@ -49,7 +58,30 @@ public class EasterEggAdapter extends RecyclerView.Adapter<EasterEggAdapter.East
         public EasterEggViewHolder(final View itemView) {
             super(itemView);
             egg = (ImageView) itemView.findViewById(R.id.image_egg);
+            egg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
 
         }
+
+    }
+    private void displayImagePopup(String link){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        builder.setPositiveButton("lol", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Log.i("EasterEgg ", "lol button Clicked!");
+//                dialog.dismiss();
+//            }
+//        });
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialoglayout = inflater.inflate(R.layout.dialog_meme_enlarged, null);
+        ImageView enlargedMeme = (ImageView) dialoglayout.findViewById(R.id.enlarged_meme);
+        Picasso.with(context).load(link).into(enlargedMeme);
+        builder.setView(dialoglayout);
+        builder.show();
     }
 }
